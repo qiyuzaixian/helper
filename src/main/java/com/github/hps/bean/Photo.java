08d095e9cbeda2c1fbe0e56dfacd207c0c8984eb
dmt <git@dmt80.com>
2019-05-13 17:58:30 +0800

    Photo

---
+++ src/main/java/com/github/hps/bean/Photo.java
@@ -0,0 +1,18 @@
+package com.github.hps.bean;
+
+/**
+ * @author ZhangChao
+ * @description
+ * @date 2019/5/13
+ */
+public class Photo {
+    private String photo;
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
